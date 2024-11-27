73019e7f14ea8836a52187842d3f29a2e1aa9b5e
mostafa96-fa <mostafaahly96@gmail.com>
2024-11-27 09:14:51 +0100

    Integrate OpenTelemetry with Flask app for tracing

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
+# syntax=docker/dockerfile:1
 FROM python:3.10-alpine
 
-WORKDIR /
+WORKDIR /test
 
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
+RUN opentelemetry-bootstrap -a install
+
 
 COPY . .
 
-CMD ["python", "web-server.py"]
+CMD ["python", "web.py"]
--- web-server.py
+++ web-server.py
@@ -1,18 +1,45 @@
 from flask import Flask
+import opentelemetry
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+import os
+
+resource = Resource(attributes={
+    SERVICE_NAME: "web-app"
+})
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(OTLPSpanExporter(ConsoleSpanExporter()))
+provider.add_span_processor(processor)
+
+# Sets the global default tracer provider
+trace.set_tracer_provider(provider)
+
+# Creates a tracer from the global tracer provider
+tracer = trace.get_tracer("server.trace")
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@tracer.start_as_current_span("healthy_server")
 def hello_geek():
     return "<h1>Hello World!</h1>", 200
 
 
 @app.route('/bad')
+@tracer.start_as_current_span("Badgateway_server")
 def stimulate_502():
     return "<h1>Bad Gateway</h1>", 502
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
+    
